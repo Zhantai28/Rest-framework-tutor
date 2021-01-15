f206b75bf2e19d7d27adf2abf99412596350cff2
Zhantai <Zhantai.Ismailov@gmail.com>
2021-01-15 14:18:52 +0600

    set up the project and added Models

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class Currency(models.Model):
+    first_curr = models.CharField(verbose_name='Валюта 1', max_length=5)
+    first_qnt = models.CharField(verbose_name='Валюта 1', max_length=65)
+    second_curr = models.CharField(verbose_name='Валюта 1', max_length=5)
+    second_qnt = models.CharField(verbose_name='Валюта 1', max_length=65)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
