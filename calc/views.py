d7ccead251247fc185f293a0b4be1f1f5b82859b
Zhantai <Zhantai.Ismailov@gmail.com>
2021-01-15 14:28:19 +0600

    added serializers and views

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CurrencySerializer
 
-# Create your views here.
+
+class CurrencyView(generics.CreateAPIView):
+    serializer_class = CurrencySerializer
