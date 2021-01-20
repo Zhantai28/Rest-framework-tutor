a5d43f010b79aa964eb8c083ca9a6b87b6752a7c
Zhantai <Zhantai.Ismailov@gmail.com>
2021-01-20 23:09:51 +0600

    Added permissions

--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .serializers import CurrencySerializer, CurrencyListSerializer
 from .models import Currency
+from .permission import IsOwnerOrReadOnly
 
 
 class CurrencyCreateView(generics.CreateAPIView):
@@ -15,3 +16,4 @@ class CurrencyListView(generics.ListAPIView):
 class CurrencyDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CurrencySerializer
     queryset = Currency.objects.all()
+    permission_classes = (IsOwnerOrReadOnly, )
