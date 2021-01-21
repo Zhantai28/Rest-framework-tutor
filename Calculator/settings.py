974fb74e14fbb5344f310cceb71b967fb09b02f6
Zhantai <Zhantai.Ismailov@gmail.com>
2021-01-21 23:48:57 +0600

    Try to work with Djoser

--- Calculator/settings.py
+++ Calculator/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'calc',
+    'djoser',
 ]
 
 MIDDLEWARE = [
